47740e670a1487a22a9e42efd51ad3bbc63421b9
pukmajster <45610204+pukmajster@users.noreply.github.com>
2023-07-23 02:33:41 +0200

    fix type error

--- src/renderer/src/components/Header.svelte
+++ src/renderer/src/components/Header.svelte
@@ -93,7 +93,7 @@
 
   <div class="flex flex-nowrap gap-2 text-sm">
     <div class="px-2 py-1 rounded-sm bg-surface-700">
-      <span class="font-bold">{$currentGameManifest.addons.length}</span> total mods
+      <span class="font-bold">{$currentGameManifest?.addons.length}</span> total mods
     </div>
     <div class="px-2 py-1 rounded-sm bg-surface-700">
       <span class="font-bold"
