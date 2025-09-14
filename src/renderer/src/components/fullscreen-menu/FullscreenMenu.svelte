f2e158aac256bfad48053db0866076ee75488e04
pukmajster <45610204+pukmajster@users.noreply.github.com>
2025-09-14 18:18:47 +0200

    adjust menu styling

--- src/renderer/src/components/fullscreen-menu/FullscreenMenu.svelte
+++ src/renderer/src/components/fullscreen-menu/FullscreenMenu.svelte
@@ -15,16 +15,24 @@
 
 <div
   class={classNames(
-    'FunkyFullscreenMenu  flex flex-row z-50  fixed left-0 top-0 bottom-0 h-screen w-screen bg-surface-900/60 text-white transition-[opacity,transform]  will-change-transform',
+    'FunkyFullscreenMenu flex flex-row z-50  fixed left-0 top-0 bottom-0 h-screen w-screen bg-surface-900/60 text-white transition-[opacity,transform]  will-change-transform',
     {
       'opacity-100 scale-100 pointer-events-auto': open,
       '-translate-x-12 opacity-0 pointer-events-none': !open
     }
   )}
 >
-  <div class="overflow-auto z-50 shadow-lg bg-surface-800/90 backdrop-blur-md min-h-screen p-3">
-    <div class="FunkyFullscreenMenu-Nav grid grid-cols-2 items-center">
-      <h3 class="text-xl indent-3 uppercase tracking-wider">{label}</h3>
+  <div
+    class="overflow-auto relative z-50 shadow-lg bg-surface-800/90 backdrop-blur-md min-h-screen"
+  >
+    <div
+      class="pointer-events-none absolute top-0 left-0 right-0 min-h-[110px] bg-gradient-to-b from-surface-400/70 to-surface-400/0 opacity-30 -z-10"
+    />
+
+    <div
+      class=" FunkyFullscreenMenu-Nav grid grid-cols-2 items-center -border-b border-surface-900 p-3"
+    >
+      <h3 class="text-lg indent-3 uppercase tracking-wider">{label}</h3>
 
       <button
         on:click={close}
@@ -34,7 +42,7 @@
       </button>
     </div>
 
-    <div class="FunkyFullscreenMenu-Panel my-4">
+    <div class="FunkyFullscreenMenu-Panel px-3">
       <slot />
     </div>
   </div>
