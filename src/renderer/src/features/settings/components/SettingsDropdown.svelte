1c39e513c3763bcfa7c5b07f71ac347b77c85497
pukmajster <45610204+pukmajster@users.noreply.github.com>
2025-09-14 03:46:39 +0200

    lessen the rounding on setting selects

--- src/renderer/src/features/settings/components/SettingsDropdown.svelte
+++ src/renderer/src/features/settings/components/SettingsDropdown.svelte
@@ -12,7 +12,7 @@
 </script>
 
 <SettingsPaper {label} {description}>
-  <select bind:value class="select !bg-surface-500 indent-2 w-auto min-w-[164px]">
+  <select bind:value class="select !bg-surface-500 indent-2 w-auto min-w-[164px] rounded">
     {#each options as option}
       <option value={option.value}>{option.label}</option>
     {/each}
